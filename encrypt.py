50229c53a23d0a37e73388a01adac94d6fa09ca4
73MrM73 <123471749+73MrM73@users.noreply.github.com>
2023-01-24 14:11:36 +0000

    Add files via upload

---
+++ encrypt.py
@@ -0,0 +1,71 @@
+alf_en = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+def dict_gen(alf_dict, k):
+    dec = {}
+    for i in range(len(alf_dict)):
+        if len(alf_dict) > i+k:
+            dec[alf_en[i]] = alf_dict[i+k]
+        elif len(alf_dict) <= i+k:
+            dec[alf_en[i]] = alf_dict[-1*(len(alf_dict)-i-k)]
+    return dec
+
+def alf(s):
+    for i in alf_en:
+        if i == s:
+            return True
+    return 'symb'
+            
+def gen_key_text(text, key):
+    count = 0
+    mas = []
+    for i in text:
+        if alf(i) == True:
+            if count < len(key):
+                mas.append(key[count])
+                count += 1
+            else:
+                count = 0
+                mas.append(key[count])
+                count += 1
+        elif alf(i) == 'symb':
+            mas.append(i)
+    return mas
+
+def gen_number(alf):
+    dec = {}
+    for i in range(len(alf)):
+        dec[alf[i]] = i 
+    return dec
+    
+def encrypt(text, key):
+    en_dict = gen_number(alf_en)
+    key_text = gen_key_text(text, key)
+    count = 0
+    mas = []
+    for i in text:
+        if alf(i) == True:
+            count2 = 0
+            for n in en_dict:
+                if n == key_text[count]:
+                    dec = dict_gen(alf_en, count2)
+                    for c in dec:
+                        if c == i:
+                             mas.append(dec[i])
+                count2 += 1
+        elif alf(i) == 'symb':    
+             mas.append(i)
+        count += 1
+    return ''.join(mas)
+            
+def main():
+    
+    key = 'BLOCKCHAIN'
+    text = 'TEST, CAR. HOUSE + CRYPTO'
+    
+    print(f"\n  Key:           {key}")
+    print(f"  Original:      {text}")
+    print(f"  Cipherogram:   {encrypt(text, key)}\n", end='')
+
+            
+if __name__ == "__main__":
+    main()
